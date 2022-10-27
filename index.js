c389de8a21e35a266687ed97ce915c8d7591a28c
Eren Demirel <l5819033@std.yildiz.edu.tr>
2022-10-27 19:51:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const getDataFirestore = require('./controller');
 
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
     getDataFirestore(res)
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is running on port 3000');
 })
